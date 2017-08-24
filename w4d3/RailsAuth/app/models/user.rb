decedef150c3b9c87ca004e75abed6d540be124a
mwei210 <m.wei210@gmail.com>
2017-08-24 08:39:48 -0700

    Adding SessionsController

---
+++ w4d3/RailsAuth/app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, presence: true
+  # validates :session_token, presence: true
+  validates :password_digest, presence: { message: 'Password \'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+end
