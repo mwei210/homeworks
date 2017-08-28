3b57aef2ad04aa67c06f597104258c036b03c9a4
mwei210 <m.wei210@gmail.com>
2017-08-27 23:21:18 -0700

    Rspec functionality

--- w4d4/RailsAuth/app/controllers/users_controller.rb
+++ w4d4/RailsAuth/app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
+
+  before_action :require_current_user!, except[:create, :new]
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user
+      login!(user)
+      redirect_to user_url
     else
       render json: @user.errors.full_messages
     end
--- w4d4/RailsAuth/app/models/user.rb
+++ w4d4/RailsAuth/app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
   attr_reader :password
 
-  validates :username, presence: true
-  # validates :session_token, presence: true
+  validates :username, :session_token, presence: true
   validates :password_digest, presence: { message: 'Password \'t be blank' }
   validates :password, length: { minimum: 6, allow_nil: true }
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
 
   def password=(password)
     @password = password
@@ -13,5 +18,15 @@ class User < ApplicationRecord
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
   end
 end
